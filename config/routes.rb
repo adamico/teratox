1b7c539592d41da620925ac21532e72453cdc896
Andrea <andrea.damico@gmail.com>
2009-10-16 20:50:15 +0200

    removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :professions
 
 
+  map.root :controller => "dossiers"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
