f2a4eeebafd72ee8e8c394447ce2efc08fce9cad
Andrea <andrea.damico@gmail.com>
2009-10-29 10:18:38 +0100

    added cucumber

---
+++ features/support/env.rb
@@ -0,0 +1,18 @@
+# Sets up the Rails environment for Cucumber
+ENV["RAILS_ENV"] ||= "cucumber"
+require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+require 'cucumber/rails/world'
+
+# Comment out the next line if you don't want Cucumber Unicode support
+require 'cucumber/formatter/unicode'
+
+require 'webrat'
+require 'cucumber/webrat/element_locator' # Lets you do table.diff!(element_at('#my_table_or_dl_or_ul_or_ol').to_table)
+
+Webrat.configure do |config|
+  config.mode = :rails
+  config.open_error_files = false # Set to true if you want error pages to pop up in the browser
+end
+
+require 'cucumber/rails/rspec'
+require 'webrat/core/matchers'
