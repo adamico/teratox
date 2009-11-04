8a08aba9adfdec5012d8f7fc173475c4a3cb2ed0
Andrea <andrea.damico@gmail.com>
2009-11-04 13:40:33 +0100

    added validations to model spec

--- spec/models/dossier_spec.rb
+++ spec/models/dossier_spec.rb
@@ -12,12 +12,13 @@ describe Dossier do
   after(:all) do
     Dossier.destroy_all
   end
-  describe "validations on create" do
+  describe "validates on create" do
     #now all specs will use make_dossier result as subject
     subject { make_dossier(:n_sicap => "LP1", :nom => "Martin") }
     should_validate_presence_of :n_sicap
     should_validate_uniqueness_of :n_sicap
     should_validate_presence_of :nom
+    should_validate_numericality_of :fcs, :ivg, :img, :miu, :geu, :nai
+    should_validate_numericality_of :sa, :less_than => 40 
   end
-
 end
