6017acf0eb8b369acc09d968953bb90cbb167fa1
Andrea <andrea.damico@gmail.com>
2009-11-06 16:11:13 +0100

    removed view specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,7 @@ Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+  config.include Webrat::Matchers, :type => :views
 
   # == Fixtures
   #
--- spec/views/dossiers/new.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "dossiers/new.html.erb" do
-  it "renders a form to create a message" do
-    dossier = Factory.stub(:dossier)
-    assigns[:dossier] = dossier
-  end
-end
