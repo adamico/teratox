f56c463c1bc6ed567ed16d6136a5053b686eb413
Andrea <andrea.damico@gmail.com>
2009-11-20 13:36:17 +0100

    finished bilan stats page for the moment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,9 @@ class HomeController < ApplicationController
 
   def bilan
     @dossiers = Dossier.all
+    @last = Dossier.last
     @count ||= @dossiers.count
+
     @age_moyen ||= Dossier.average(:age).round(1)
     @age_sd ||= Math.sqrt(
       Dossier.sum(
@@ -13,10 +15,19 @@ class HomeController < ApplicationController
       ).to_f / @count
     ).round(1)
     @age_lt35 = Dossier.age_lt(35).count
+
+    @sa_moyen ||= Dossier.average(:sa).round(1)
+    @sa_sd ||= Math.sqrt(
+      Dossier.sum(
+        "(sa - #{@sa_moyen}) * (sa - #{@sa_moyen})"
+      ).to_f / @count
+    ).round(1)
+
     @p1g1 = Dossier.fcs_is(0).ivg_is(0).img_is(0).miu_is(0).geu_is(0).nai_is(0).count
-    @last = Dossier.last
+
     @solvants ||= Dossier.solvants
     @solv_count ||= @solvants.count
+
     @solvage_moyen ||= @solvants.average(:age).round(1)
     @solvage_sd ||= Math.sqrt(
       @solvants.sum(
@@ -24,10 +35,23 @@ class HomeController < ApplicationController
       ).to_f / @solv_count
     ).round(1)
     @solvage_lt35 = @solvants.age_lt(35).count
+
+    @solvsa_moyen ||= @solvants.average(:sa).round(1)
+    @solvsa_sd ||= Math.sqrt(
+      @solvants.sum(
+        "(sa - #{@solvsa_moyen}) * (sa - #{@solvsa_moyen})"
+      ).to_f / @solv_count
+    ).round(1)
+
     @solv_p1g1 = @solvants.fcs_is(0).ivg_is(0).img_is(0).miu_is(0).geu_is(0).nai_is(0).count
-    @autres_nr = @dossiers.count - @solvants.count
+
+    @autres_count = @dossiers.count - @solvants.count
     @autres_age_total = Dossier.sum(:age) - @solvants.sum(:age)
-    @autres_age_moyen ||= (@autres_age_total / @autres_nr).to_f
+    @autres_age_moyen ||= (@autres_age_total / @autres_count).to_f
+
+    @autres_sa_total = Dossier.sum(:sa) - @solvants.sum(:sa)
+    @autres_sa_moyen ||= (@autres_sa_total / @autres_count).to_f
+
     @evolutions ||= Acctype.all
     @bebes ||= Bebe.all
     @niveaux ||= Niveau.all
