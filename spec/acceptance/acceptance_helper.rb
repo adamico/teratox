84504f0f24a79cd9dbc65095c60152015f722add
Andrea <andrea.damico@gmail.com>
2010-10-05 16:28:05 +0200

    adding capybara acceptance_helper for steak

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -7,12 +7,4 @@ RSpec.configuration.include Capybara, :type => :acceptance
 # Put your acceptance spec helpers inside /spec/acceptance/support
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-RSpec.configure do |config|
-  config.before(:each) do
-    Capybara.current_driver = :selenium if example.metadata[:js]
-  end
 
-  config.after(:each) do
-    Capybara.use_default_driver if example.metadata[:js]
-  end
-end
