b1cfd323d91384fbcc76f68290fe2e48be38f9a4
Andrea <andrea.damico@gmail.com>
2010-11-29 18:06:36 +0100

    tweaks for counts vs length, tweaked remaining controllers

--- app/controllers/qualites_controller.rb
+++ app/controllers/qualites_controller.rb
@@ -1,44 +1,36 @@
 class QualitesController < ApplicationController
+  load_and_authorize_resource
+
   def index
-    @qualites = Qualite.all
   end
 
   def show
-    @qualite = Qualite.find(params[:id])
   end
-  
+
   def new
-    @qualite = Qualite.new
   end
 
   def create
-    @qualite = Qualite.new(params[:qualite])
     if @qualite.save
-      flash[:notice] = "Successfully created qualite."
-      redirect_to @qualite
+      redirect_to qualites_url, :notice => @flash_message
     else
       render :action => 'new'
     end
   end
 
   def edit
-    @qualite = Qualite.find(params[:id])
   end
 
   def update
-    @qualite = Qualite.find(params[:id])
     if @qualite.update_attributes(params[:qualite])
-      flash[:notice] = "Successfully updated qualite."
-      redirect_to @qualite
+      redirect_to qualites_url, :notice => @flash_message
     else
       render :action => 'edit'
     end
   end
 
   def destroy
-    @qualite = Qualite.find(params[:id])
     @qualite.destroy
-    flash[:notice] = "Successfully destroyed qualite."
-    redirect_to qualites_url
+    redirect_to qualites_url, :notice => @flash_message
   end
 end
--- app/controllers/specialites_controller.rb
+++ app/controllers/specialites_controller.rb
@@ -1,44 +1,36 @@
 class SpecialitesController < ApplicationController
+  load_and_authorize_resource
+
   def index
-    @specialites = Specialite.all
   end
-  
+
   def show
-    @specialite = Specialite.find(params[:id])
   end
-  
+
   def new
-    @specialite = Specialite.new
   end
-  
+
   def create
-    @specialite = Specialite.new(params[:specialite])
     if @specialite.save
-      flash[:notice] = "Successfully created specialite."
-      redirect_to @specialite
+      redirect_to specialites_url, :notice => @flash_message
     else
       render :action => 'new'
     end
   end
-  
+
   def edit
-    @specialite = Specialite.find(params[:id])
   end
-  
+
   def update
-    @specialite = Specialite.find(params[:id])
     if @specialite.update_attributes(params[:specialite])
-      flash[:notice] = "Successfully updated specialite."
-      redirect_to @specialite
+      redirect_to specialites_url, :notice => @flash_message
     else
       render :action => 'edit'
     end
   end
-  
+
   def destroy
-    @specialite = Specialite.find(params[:id])
     @specialite.destroy
-    flash[:notice] = "Successfully destroyed specialite."
-    redirect_to specialites_url
+    redirect_to specialites_url, :notice => @flash_message
   end
 end
